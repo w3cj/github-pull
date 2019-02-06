341de0f1204ea3f4d6a560bff959abe75899d042
CJ R <cj@null.computer>
2019-02-06 12:38:08 -0700

    :tada: initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Github Webhook Pull Server
+
+An express server that will pull the latest changes to a git repo when it receives an incoming request from github.
+
+## Setup
+
+```sh
+cp .env.example .env
+```
+
+Update DIRECTORY variable with absolute path to folder with git repo in it.
---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { exec } = require('child_process');
+
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'github webhook server'
+  });
+});
+
+app.post('/github', (req, res) => {
+  console.log(req.body);
+  exec('cd ${process.env.DIRECTORY} && git pull', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+    res.json({
+      message: 'success'
+    });
+  });
+});
+
+const port = process.env.PORT || 9000;
+app.listen(port, () =>{
+  console.log(`Listening on ${port}`);
+});
